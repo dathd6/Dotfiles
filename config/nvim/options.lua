ec6546048ae6bf67cd9cebf2fc42fefc810e246e
dathd6 <dr.dathd@gmail.com>
2023-07-03 22:22:45 +0700

    Add gitignore

--- config/nvim/options.lua
+++ config/nvim/options.lua
@@ -2,13 +2,12 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true,              -- sets vim.opt.relativenumber
-    number = true,                      -- sets vim.opt.number
-    spell = false,                      -- sets vim.opt.spell
-    signcolumn = "auto",                -- sets vim.opt.signcolumn to auto
-    wrap = false,                       -- sets vim.opt.wrap
-    directory = vim.fn.expand("%:p:h"), -- sets swap file directory
-    showtabline = 0,                    -- disable tab line
+    relativenumber = true, -- sets vim.opt.relativenumber
+    number = true,         -- sets vim.opt.number
+    spell = false,         -- sets vim.opt.spell
+    signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
+    wrap = false,          -- sets vim.opt.wrap
+    showtabline = 0,       -- disable tab line
   },
   g = {
     mapleader = " ",                 -- sets vim.g.mapleader
